cd8d5c33a85264ff9db834328cc0a995dfb166cc
6Morpheus6 <wanton-decay@gmx.net>
2025-09-23 02:55:16 +0200

    gpu specific start flags

--- start.js
+++ start.js
@@ -2,21 +2,58 @@ module.exports = {
   daemon: true,
   run: [
     {
+      when: "{{platform === 'linux' && gpu === 'nvidia' && kernel.gpus && kernel.gpus.find(x => / 50.+/.test(x.model))}}",
       method: "shell.run",
       params: {
         venv: "env",
         env: { },
         path: "app",
         message: [
-          "python webui.py --host 127.0.0.1",
+          "python webui.py --host 127.0.0.1 --cuda_kernel",
         ],
         on: [{
           "event": "/http:\/\/\\S+/",
           "done": true
         }]
-      }
+      },
+      next: "uri"
     },
     {
+      when: "{{gpu !== 'nvidia'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",
+        env: { },
+        path: "app",
+        message: [
+          "python webui.py --host 127.0.0.1 ",
+        ],
+        on: [{
+          "event": "/http:\/\/\\S+/",
+          "done": true
+        }]
+      },
+      next: "uri"
+    },
+    {
+      when: "{{gpu === 'nvidia'}}",
+      method: "shell.run",
+      params: {
+        venv: "env",
+        env: { },
+        path: "app",
+        message: [
+          "python webui.py --host 127.0.0.1 --cuda_kernel --deepspeed",
+        ],
+        on: [{
+          "event": "/http:\/\/\\S+/",
+          "done": true
+        }]
+      },
+      next: "uri"
+    },
+    {
+      id: "uri",
       method: "local.set",
       params: {
         url: "{{input.event[0]}}"
